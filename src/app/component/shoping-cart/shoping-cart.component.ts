172465b03e6327d7feb34f82eef57e53715f7dfd
adam20092001 <adam20092001@gmail.com>
2023-05-02 22:43:09 -0500

    carrito de compras

--- src/app/component/shoping-cart/shoping-cart.component.ts
+++ src/app/component/shoping-cart/shoping-cart.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-shoping-cart',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./shoping-cart.component.css']
 })
 export class ShopingCartComponent {
+  lista: Product[] = [];
+  displayedColumns=['name','price'];
+  dataSource= new MatTableDataSource<Product>();
+  constructor(private productService: ProductService, private router: Router){
+  }
+  ngOnInit(): void {
+    this.productService.list().subscribe(data =>
+      this.dataSource.data=data);
+  }
+  getTotalCost() {
+    return this.dataSource.data.map(t => t.price).reduce((acc, value) => acc + value, 0);
+  }
 
 }
